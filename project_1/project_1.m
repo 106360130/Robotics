fd235c1a0684f7c7aa261f6c13d712789938cabc
106360130 <bs25017@gmail.com>
2021-11-16 17:39:15 +0800

    Inverse kinematics

--- project_1/project_1.m
+++ project_1/project_1.m
@@ -17,4 +17,11 @@ alpha=[-90, 0, 0, -90, 90, 0];
 i = 6;
 T_result = T_coe(d(i), a(i), alpha(i))
 
-theta_inverse = kinematics_inverse(T)*(180/pi)
+theta_inverse_1 = kinematics_inverse_1(T)*(180/pi)
+theta_inverse_2 = kinematics_inverse_2(T)*(180/pi)
+theta_inverse_3 = kinematics_inverse_3(T)*(180/pi)
+theta_inverse_4 = kinematics_inverse_4(T)*(180/pi)
+theta_inverse_5 = kinematics_inverse_5(T)*(180/pi)
+theta_inverse_6 = kinematics_inverse_6(T)*(180/pi)
+theta_inverse_7 = kinematics_inverse_7(T)*(180/pi)
+theta_inverse_8 = kinematics_inverse_8(T)*(180/pi)
