17b7dd3a5d7b44358843782f608c80d600529401
Jakob <jakob1519.ee10@nycu.edu.tw>
2021-12-31 20:40:51 +0800

    Joint motion and Cartesian motion

--- project_2/Reference/inverse_kinematics/portion_liner.m
+++ project_2/Reference/inverse_kinematics/portion_liner.m
@@ -3,9 +3,9 @@ function [q dq ddq] = portion_liner(time_interval, sample_rate, T, delta, theta_
     i=1;
     for sample_time = time_interval(1) : sample_rate : time_interval(2)
         h = sample_time/T;
-        q(:,i) = delta*h + theta_B;
-        dq(:,i) = delta/T;
-        ddq(:,i) = [0; 0; 0; 0; 0; 0];
+        q(:, i) = delta*h + theta_B;
+        dq(:, i) = delta/T;
+        ddq(:, i) = [0; 0; 0; 0; 0; 0];
         
         i = i+1;
 
---
+++ project_2/Reference/inverse_kinematics_2/portion_accelerated.m
@@ -0,0 +1,21 @@
+function [q dq ddq] = portion_accelerated(time_interval, sample_rate, T, t_acc, noap_A, noap_B, noap_C)
+
+    delta_B = noap_A - noap_B;
+    delta_C = noap_C - noap_B;
+
+    i = 1;
+    for sample_time = (time_interval(1) + sample_rate) : sample_rate : (time_interval(2) - sample_rate)
+
+        temp_t = time_interval(2) - time_interval(1);
+        h = (sample_time + t_acc)/temp_t;
+        temp_1 = (delta_C*(t_acc/T) + delta_B);
+    
+        q(:, :, i) = (temp_1*(2-h)*h^2 - 2*delta_B)*h + noap_B + delta_B;  
+        dq(:, :, i) = (temp_1*(1.5-h)*2*h^2 - delta_B) / t_acc;
+        ddq(:, :, i) = temp_1*(1-h)*3*h / t_acc^2;
+    
+        i = i+1;
+    
+    end
+
+end
